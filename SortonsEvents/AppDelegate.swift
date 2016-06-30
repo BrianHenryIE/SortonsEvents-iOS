2621e4d4b4558e1d816a4eeda7f7608f4e168ec1
Brian Henry <BrianHenryIE@gmail.com>
2016-06-30 21:49:01 +0100

    Moving to Swift!!
    Core bits

---
+++ SortonsEvents/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SortonsEvents
+//
+//  Created by Brian Henry on 12/03/2016.
+//  Copyright © 2016 Sortons. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+//    [AFNetworkActivityIndicatorManager sharedManager].enabled = YES;
+//    
+//    // Set the application defaults
+//    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+//    NSDictionary *appDefaults = [NSDictionary dictionaryWithObject:@"YES"
+//    forKey:@"launch_native_apps_toggle"];
+//    [defaults registerDefaults:appDefaults];
+//    [defaults synchronize];
+    
+    var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        return true
+    }
+    
+}
