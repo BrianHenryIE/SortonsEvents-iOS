ae5aae36f90bde9f72cb1713f146081271e6ce99
Brian Henry <BrianHenryIE@gmail.com>
2016-10-04 00:28:18 -0700

    plist id parsing

---
+++ SortonsEventsTests/FomoIdTest.swift
@@ -0,0 +1,42 @@
+//
+//  FomoIdTest.swift
+//  SortonsEvents
+//
+//  Created by Brian Henry on 03/10/2016.
+//  Copyright © 2016 Sortons. All rights reserved.
+//
+
+import XCTest
+
+@testable import SortonsEvents
+
+class FomoIdTest: XCTestCase {
+    
+    func testFomoId() {
+   
+        var actual : String
+        var expected : String
+       
+        actual = FomoId.numberFromBundle(bundleName: "ie.sortons.events.nuig")!
+        expected = "1049082365115363"
+        XCTAssertEqual(expected, actual)
+            
+        actual = FomoId.numberFromBundle(bundleName: "ie.sortons.events.qub")!
+        expected = "481772365334307"
+        XCTAssertEqual(expected, actual)
+        
+        actual = FomoId.numberFromBundle(bundleName: "ie.sortons.events.ucd")!
+        expected = "428055040731753"
+        XCTAssertEqual(expected, actual)
+        
+        actual = FomoId.numberFromBundle(bundleName: "ie.sortons.events.dit")!
+        expected = "1484790081833630"
+        XCTAssertEqual(expected, actual)
+        
+        actual = FomoId.numberFromBundle(bundleName: "ie.sortons.events.tcd")!
+        expected = "884332671653875"
+        XCTAssertEqual(expected, actual)
+        
+    }
+        
+}
