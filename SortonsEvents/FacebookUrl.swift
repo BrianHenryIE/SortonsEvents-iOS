b1cda7d3c3f6efc17bc483a9e96b22deb1dab50a
Brian Henry <BrianHenryIE@gmail.com>
2017-01-25 00:13:00 -0800

    regex swift 3 syntax update

--- SortonsEvents/FacebookUrl.swift
+++ SortonsEvents/FacebookUrl.swift
@@ -75,7 +75,7 @@ extension String {
     func matchingStrings(_ regex: String) -> [[String]] {
         guard let regex = try? NSRegularExpression(pattern: regex, options: []) else { return [] }
         let nsString = self as NSString
-        let results  = regex.matches(in: self, options: [], range: NSMakeRange(0, nsString.length))
+        let results  = regex.matches(in: self, range: NSRange(location: 0, length: nsString.length))
         return results.map { result in
             (0..<result.numberOfRanges).map { result.rangeAt($0).location != NSNotFound
                 ? nsString.substring(with: result.rangeAt($0))
